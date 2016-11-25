c271cd89daca73f03fc954cf85f048290483bbb4
Bartłomiej Błazik <Bartomiej Bazik>
2016-11-25 16:17:45 +0100

    scanner and endpoint

---
+++ app/src/main/java/com/example/barte_000/rajdownik/API.java
@@ -0,0 +1,77 @@
+package com.example.barte_000.rajdownik;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by barte_000 on 19.11.2016.
+ */
+
+
+
+public class API {
+
+    private static APIInterface apiInterface;
+    private static String url = "http://frelia.org:3002/api/v1/offers/";
+    public static String uid = "";
+    public static String token = "";
+    public static String client = "";
+
+    public static APIInterface getClient() {
+        if (apiInterface == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(
+                            new Interceptor() {
+                                @Override
+                                public Response intercept(Chain chain) throws IOException {
+                                    Request request = chain.request().newBuilder()
+                                            .addHeader("Accept", "Application/JSON")
+                                            .addHeader("uid", uid)
+                                            .addHeader("client", client)
+                                            .addHeader("access-token",token).build();
+                                    return chain.proceed(request);
+                                }
+                            }).build();
+
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                    .create();
+
+            Retrofit client = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .client(okHttpClient)
+                    .build();
+            apiInterface = client.create(APIInterface.class);
+        }
+        return apiInterface;
+    }
+
+
+    public interface APIInterface {
+
+        @POST("/api/v1/offers/")
+        Call<String> sendIndexNumber(@Body String indexNumber);
+
+    }
+}
+
--- app/src/main/java/com/example/barte_000/rajdownik/Scanner.java
+++ app/src/main/java/com/example/barte_000/rajdownik/Scanner.java
@@ -2,12 +2,80 @@ package com.example.barte_000.rajdownik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class Scanner extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class Scanner extends AppCompatActivity implements OnClickListener{
+
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
+    private API.APIInterface apiInterface;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        apiInterface = API.getClient();
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        //respond to clicks
+        if(v.getId()==R.id.scan_button){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+
+            Call<String> call = apiInterface.sendIndexNumber(scanContent);
+            call.enqueue(new Callback<String>() {
+
+                @Override
+                public void onResponse(Call<String> call, retrofit2.Response<String> response) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Index sended!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Host not responding!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 }
